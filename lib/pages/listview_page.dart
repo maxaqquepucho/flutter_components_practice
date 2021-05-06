71ee016b88d47e7b94bf01ef6e94ce8adf6b773f
Max Aqquepucho <max.aqquepucho@global66.com>
2021-05-06 09:06:02 -0500

    Add pull refresh

--- lib/pages/listview_page.dart
+++ lib/pages/listview_page.dart
@@ -50,18 +50,33 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   Widget _crearLista() {
-    return ListView.builder(
-      controller: _scrollController,
-      itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext context, int index) {
-        final image = _listaNumeros[index];
-        String urlAPI = 'https://picsum.photos/200/100/?image=$image';
-        return FadeInImage(
-          image: NetworkImage(urlAPI),
-          placeholder: AssetImage('assets/jar-loading.gif'),
-        );
-      },
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index) {
+          final image = _listaNumeros[index];
+          String urlAPI = 'https://picsum.photos/200/100/?image=$image';
+          return FadeInImage(
+            image: NetworkImage(urlAPI),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+          );
+        },
+      ),
     );
+  }
+
+  Future<Null> obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+
+    Timer(duration, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duration);
   }
 
   void _agregar10() {
